2d7632e496c4f0edcb2ddbd9005b857bdc53c6f3
Klaster28 <89096914521@mail.ru>
2022-09-23 21:14:47 +0300

    Коммит после добавления Box

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 123456789;
-        System.out.println("Значение переменной 'x' с типом данных 'int' равно " + x);
-        byte y = 100;
-        System.out.println("Значение переменной 'y' стипом данных 'byte' равно " + y);
-        short z = 12345;
-        System.out.println("Значение переменной 'z' стипом данных 'short' равно " + z);
-        long a = 111222333444L;
-        System.out.println("Значение переменной 'a' стипом данных 'long' равно " + a);
-        double b = -1.1+200;
-        System.out.println("Значение переменной 'b' стипом данных 'duble' равно " + b);
-        float c = 2.3f;
-        System.out.println("Значение переменной 'c' стипом данных 'float' равно " + c);
+        int xBox = 123456789;
+        System.out.println("Значение переменной 'xBox' с типом данных 'int' равно " + xBox);
+        byte yBox = 100;
+        System.out.println("Значение переменной 'yBox' стипом данных 'byte' равно " + yBox);
+        short zBox = 12345;
+        System.out.println("Значение переменной 'zBox' стипом данных 'short' равно " + zBox);
+        long aBox = 111222333444L;
+        System.out.println("Значение переменной 'aBox' стипом данных 'long' равно " + aBox);
+        double bBox = -1.1+200;
+        System.out.println("Значение переменной 'bBox' стипом данных 'duble' равно " + bBox);
+        float cBox = 2.3f;
+        System.out.println("Значение переменной 'cBox' стипом данных 'float' равно " + cBox);
 
 
 
