7c051095f94d44c375bad9e05779893fe77e782a
Klaster28 <89096914521@mail.ru>
2022-09-23 20:47:50 +0300

    Первый коммит в переменных 4

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 123456789;
+        System.out.println("Значение переменной 'x' с типом данных 'int' равно " + x);
+        byte y = 100;
+        System.out.println("Значение переменной 'y' стипом данных 'byte' равно " + y);
+
+    }
+}
