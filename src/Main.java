3334a200aa28517a7db24ccc9359c0fdcd834490
Klaster28 <89096914521@mail.ru>
2022-09-23 21:10:26 +0300

    Коммит после первого задания

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,16 @@ public class Main {
         System.out.println("Значение переменной 'x' с типом данных 'int' равно " + x);
         byte y = 100;
         System.out.println("Значение переменной 'y' стипом данных 'byte' равно " + y);
+        short z = 12345;
+        System.out.println("Значение переменной 'z' стипом данных 'short' равно " + z);
+        long a = 111222333444L;
+        System.out.println("Значение переменной 'a' стипом данных 'long' равно " + a);
+        double b = -1.1+200;
+        System.out.println("Значение переменной 'b' стипом данных 'duble' равно " + b);
+        float c = 2.3f;
+        System.out.println("Значение переменной 'c' стипом данных 'float' равно " + c);
+
+
 
     }
 }
