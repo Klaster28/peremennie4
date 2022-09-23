921fa907d4eb4690dfb566675b9b19946b3e1f57
Klaster28 <89096914521@mail.ru>
2022-09-23 21:29:35 +0300

    Коммит после второго задания

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,39 @@ public class Main {
         int xBox = 123456789;
         System.out.println("Значение переменной 'xBox' с типом данных 'int' равно " + xBox);
         byte yBox = 100;
-        System.out.println("Значение переменной 'yBox' стипом данных 'byte' равно " + yBox);
+        System.out.println("Значение переменной 'yBox' с типом данных 'byte' равно " + yBox);
         short zBox = 12345;
-        System.out.println("Значение переменной 'zBox' стипом данных 'short' равно " + zBox);
+        System.out.println("Значение переменной 'zBox' с типом данных 'short' равно " + zBox);
         long aBox = 111222333444L;
-        System.out.println("Значение переменной 'aBox' стипом данных 'long' равно " + aBox);
+        System.out.println("Значение переменной 'aBox' с типом данных 'long' равно " + aBox);
         double bBox = -1.1+200;
-        System.out.println("Значение переменной 'bBox' стипом данных 'duble' равно " + bBox);
+        System.out.println("Значение переменной 'bBox' с типом данных 'duble' равно " + bBox);
         float cBox = 2.3f;
-        System.out.println("Значение переменной 'cBox' стипом данных 'float' равно " + cBox);
+        System.out.println("Значение переменной 'cBox' с типом данных 'float' равно " + cBox);
+
+        float x = 27.12f;
+        System.out.println("x с типом данных 'float' равен " + x);
+        long y = 987678965549L;
+        System.out.println("y с типом данных 'long' равен " + y);
+        byte z = 2;
+        System.out.println("z с типом данных 'byte' равен " + z);
+        short q = 786;
+        System.out.println("q с типом данных 'short' равен " + q);
+        boolean w = false;
+        System.out.println("w с типом данных 'boolean' равен " + w);
+        short e = 569;
+        System.out.println("e с типом данных 'short' равен " + e);
+        short r = -159;
+        System.out.println("r с типом данных 'short' равен " + r);
+        short t = 27897;
+        System.out.println("t с типом данных 'short' равен " + t);
+        byte s = 67;
+        System.out.println("s с типом данных 'byte' равен " + s);
+
+
+
+
+
 
 
 
